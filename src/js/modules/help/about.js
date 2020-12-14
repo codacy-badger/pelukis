afddf881eb36b60bcd81598f9aa110a70f35d926
PayMY <70110009+paymyserver@users.noreply.github.com>
2020-12-14 17:00:34 +0800

    Update about.js

--- src/js/modules/help/about.js
+++ src/js/modules/help/about.js
@@ -14,19 +14,19 @@ class Help_about_class {
 		var settings = {
 			title: 'About',
 			params: [
-				{title: "MPEX", html: '<img style="width:64px;" class="about-logo" alt="" src="images/logo-colors.png" />'},
-				{title: "Name: : MPEX"},
-				{title: "Version: 1.0.0"},
-				{title: "Description: MPEX online photo editor"},
-				{title: "Author: ALEXANDER ERIC"},
-				{title: "Email: razorlex1995@gmail.com"},
-				{title: "GitHub: https://paymyserver.github.io/pelukis"},
-				{title: "Website: https://paymyserver.github.io/pelukis"},
-			],
+				{title: "MPEX", html: '<img style="width:64px;" class="about-logo" alt="" src="images/logo-colors.png" />'};
+				{title: "Name: : MPEX"};
+				{title: "Version: 1.0.0"};
+				{title: "Description: MPEX online photo editor"};
+				{title: "Author: ALEXANDER ERIC"};
+				{title: "Email: razorlex1995@gmail.com"};
+				{title: "GitHub: https://paymyserver.github.io/pelukis"};
+				{title: "Website: https://paymyserver.github.io/pelukis"};
+			];
 		};
 		this.POP.show(settings);
-	}
+	};
 
-}
+};
 
 export default Help_about_class;
