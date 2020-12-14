24d48e02b9d2dd126d512810ed6fa82badae0cf7
PayMY <70110009+paymyserver@users.noreply.github.com>
2020-12-14 16:19:48 +0800

    Add files via upload

---
+++ src/js/modules/layer/duplicate.js
@@ -0,0 +1,64 @@
+import config from './../../config.js';
+import Base_layers_class from './../../core/base-layers.js';
+import Helper_class from './../../libs/helpers.js';
+
+var instance = null;
+
+class Layer_duplicate_class {
+
+	constructor() {
+		//singleton
+		if (instance) {
+			return instance;
+		}
+		instance = this;
+
+		this.Base_layers = new Base_layers_class();
+		this.Helper = new Helper_class();
+
+		this.set_events();
+	}
+
+	set_events() {
+		document.addEventListener('keydown', (event) => {
+			var code = event.keyCode;
+			if (this.Helper.is_input(event.target))
+				return;
+
+			if (code == 68) {
+				//D - duplicate
+				this.duplicate();
+				event.preventDefault();
+			}
+		}, false);
+	}
+
+	duplicate() {
+		window.State.save();
+
+		var params = JSON.parse(JSON.stringify(config.layer));
+		delete params.id;
+		delete params.order;
+		params.name = "Copy: " + params.name;
+		if(params.x != 0 || params.y != 0 || params.width != config.WIDTH || params.height != config.HEIGHT){
+			params.x += 10;
+			params.y += 10;
+		}
+
+		for (var i in params) {
+			//remove private attributes
+			if (i[0] == '_')
+				delete params[i];
+		}
+
+		if (params.type == 'image') {
+			//image
+			params.link = config.layer.link.cloneNode(true);
+		}
+
+		this.Base_layers.insert(params);
+	}
+
+}
+
+export default Layer_duplicate_class;
