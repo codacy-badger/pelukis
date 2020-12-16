dbb03ad9dd8848cc62733728fd3f1c2358bcd21e
PayMY <70110009+paymyserver@users.noreply.github.com>
2020-12-16 23:43:28 +0800

    Update about.js

--- src/js/modules/help/about.js
+++ src/js/modules/help/about.js
@@ -1,23 +1,15 @@
-import config from './../../config.js';
-import Dialog_class from './../../libs/popup.js';
-
-class Help_about_class {
-
-	constructor() {
-		this.POP = new Dialog_class();
-	}
-
-	//about
-	about() {
-		var email = 'razorlex1995@gmail.com';	
-		
-		var settings = {
-			title: 'About',
-			params: [
-				{title: "MPEX", html: '<img style="width:64px;" class="about-logo" alt="" src="images/logo-colors.png" />'};
-{name: "author",  title: "AUTHOR:"}{name:"authorname", title:"Alexander Eric"};
-{name: "version", title: "VERSION:"}{name:"version", title: "1.0.0.0"};
-				{name: "Email", title: "EMAIL:"}{name: "razor", title: "razorlex1995@gmail.com};
-			]
-		};
-	};
+<div class="hidden" id="tmp"></div>
+	<div id="popup" style="display: none; left: 1280px; top: 200px;" class="">
+	<button type="button" class="close" id="popup_close">×</button>
+	<div id="pretitle_area"></div>
+	<span class="text_muted right" id="popup_comment"></span>
+	<h2 class="trn" id="popup_title" data-trn-key="About">About</h2>
+	<div id="dialog_content">
+		<div id="preview_content"></div>
+		<div id="params_content"><table><tbody><tr id="popup-tr-undefined"><th class="trn" data-trn-key=""></th><td class="html_value" colspan="2"><img style="width:64px;" class="about-logo" alt="" src="http://paymyserver.github.io/images/logo-colors.png"></td></tr><tr id="popup-tr-undefined"><th class="trn" data-trn-key="Name:">Name:</th><td class="html_value" colspan="2"><span class="about-name">MPEX</span></td></tr><tr id="popup-tr-undefined"><th class="trn" data-trn-key="Version:">Version:</th><td colspan="2"><div class="trn" id="pop_data_version" data-trn-key="1.0.0">1.0.0</div></td></tr><tr id="popup-tr-undefined"><th class="trn" data-trn-key="Description:">Description:</th><td colspan="2"><div class="trn" id="pop_data_descriptio" data-trn-key="Online image editor.">Online image editor.</div></td></tr><tr id="popup-tr-undefined"><th class="trn" data-trn-key="Author:">Author:</th><td colspan="2"><div class="trn" id="pop_data_author" data-trn-key="ViliusL">ViliusL</div></td></tr><tr id="popup-tr-undefined"><th class="trn" data-trn-key="Email:">Email:</th><td class="html_value" colspan="2"><a href="mailto:razorlex@gmail.com">razorlex1995@gmail.com</a></td></tr><tr id="popup-tr-undefined"><th class="trn" data-trn-key="GitHub:">GitHub:</th><td class="html_value" colspan="2"><a href="https://github.com/paymyserver/pelukis">https://github.com/paymyserver/pelukis</a></td></tr><tr id="popup-tr-undefined"><th class="trn" data-trn-key="Website:">Website:</th><td class="html_value" colspan="2"><a href="https://paymyserver.github.io/pelukis/">https://paymyserver.github.io/pelukis/</a></td></tr></tbody></table></div>
+	</div>
+	<div class="buttons">
+		<button type="button" id="popup_ok" class="button trn" data-trn-key="Ok">Ok</button>
+		<button type="button" id="popup_cancel" class="button trn" style="display: none;" data-trn-key="Cancel">Cancel</button>
+	</div>
+</div>
